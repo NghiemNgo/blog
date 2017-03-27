0392a045978eb46a3bef901334034674f37b992f
Ngo Van Nghiem <nghiem.ngo22@gmail.com>
2017-03-27 17:46:55 +0700

    update content home page

--- storage/framework/views/f6eba72d6e20d9e16896355525210082a34390b7.php
+++ storage/framework/views/f6eba72d6e20d9e16896355525210082a34390b7.php
@@ -1,16 +1,61 @@
+<link href="<?php echo e(asset('/css/home.css')); ?>" rel="stylesheet"> 
 <?php $__env->startSection('content'); ?>
-<div class="container">
-    <div class="row">
-<!--        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    You are logged in!
+<div class="entry-content">
+    <section class="section-top-page">
+        <div class="wrapper">
+            <div class="blog">
+                <h1>We will continue to keep growing in order to become the leading Vietnamese software company.</h1>
+                <h2>We are Rikkeisoft – where the dream begins!</h2>
+            </div>
+        </div>
+    </section>
+    <section class="section-second-page">
+        <div class="wrapper">
+            <p class="title-about-us"><u>About Us</u></p>
+            <div class="blog">
+                <p class="content-about-us">On April 6, 2012, Rikkeisoft was founded in Hanoi, Vietnam by 5 Vietnamese graduates of Ritsumeikan University and Keio University, both located in Japan; the company name is derived from the alma maters of the founders. Rikkeisofts team consists exclusively of graduates from the highest ranking universities in Japan and Vietnam. Next to this, some of our team, including the founders, have a variety of other achievements, such as winning the Mathematical Olympiad and IT Olympics.</p>
+            </div>
+        </div>
+    </section>
+    <section class="section-third-page">
+        <div class="wrapper">
+            <p class="title-products"><u>Our Products</u></p>
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="row">
+                        <div class="col-md-3"><image class="icon-mobile" src="/images/mobile-icon.png"/></div>
+                        <div class="col-md-9"><p class="icon-mobile"> SMARTPHONE<br />APPLICATTION</p></div>
+                    </div>
+                    <p class="content-icon-mobile">Since Rikkeisoft has the exact know-how in development of games and apps, we will find the best solution for you.</p>
+                    <p><a href="http://en.rikkeisoft.com/product/game-development/">read more</a></p>
+                </div>
+                <div class="col-md-3">
+                    <div class="row">
+                        <div class="col-md-3"><image class="icon-mobile" src="/images/game-icon.png"/></div>
+                        <div class="col-md-9"><p class="icon-mobile">GAME<br />DEVELOPMENT</p></div>
+                    </div>
+                    <p class="content-icon-mobile">The games are tailor-made, resulting in exactly the gameplay, style and layout that you require!</p>
+                    <p><a href="http://en.rikkeisoft.com/product/smartphone-application/">read more</a></p>
+                </div>
+                <div class="col-md-3">
+                    <div class="row">
+                        <div class="col-md-3"><image class="icon-mobile" src="/images/web-icon.png"/></div>
+                        <div class="col-md-9"><p class="icon-mobile">WEB<br />SYSTEMS</p></div>
+                    </div>
+                    <p class="content-icon-mobile">In previous experiences, we have gained experience in developing on a variety of scales and systems, resulting in being able to create the perfect web system for you.</p>
+                    <p><a href="http://en.rikkeisoft.com/product/web-systems/">read more</a></p>
+                </div>
+                <div class="col-md-3">
+                    <div class="row">
+                        <div class="col-md-3"><image class="icon-mobile" src="/images/business-icon.png"/></div>
+                        <div class="col-md-9"><p class="icon-mobile">BUSINESSPROCESS<br />SYSTEM</p></div>
+                    </div>
+                    <p class="content-icon-mobile">To ensure usability and security, the experienced team of Rikkeisoft will respond quickly to any comments or suggestion from your side, resulting in high quality from the basic design to the final system test!</p>
+                    <p><a href="http://en.rikkeisoft.com/product/business-process-system/">read more</a></p>
                 </div>
             </div>
-        </div>-->
-    </div>
+        </div>
+    </section>
 </div>
 <?php $__env->stopSection(); ?>
 
