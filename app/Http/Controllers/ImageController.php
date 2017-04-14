5da66ead9efd73ab1e0ffd16a256c57e55429863
Ngo Van Nghiem <nghiem.ngo22@gmail.com>
2017-04-14 15:34:16 +0700

    create edit image

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -3,10 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
-
 use App\Model\Image;
+use File;
 
 class ImageController extends Controller
 {
@@ -69,13 +68,31 @@ class ImageController extends Controller
 
     /**
      * Show the form for editing the specified resource.
+     * fists: get id
+     * seconds, move uploaded file to source
+     * thirds: delete old file
+     * finally: update
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit($file, $id)
     {
-        //
+        $destinationPath = public_path().'/images';   
+        $baseName = $file->getClientOriginalName();     // get base name file
+        $fileName = time() . '-' . $baseName;           //create new name file
+        $upload_success = $file->move($destinationPath, $fileName);
+        if($upload_success) {
+            $upload_image = Image::findOrFail($id);
+            if($upload_image->image) {
+                File::delete('images/' . $upload_image->image); // delete old file
+            }
+            //save url image
+            $upload_image->image = $fileName;
+            $upload_image->save();
+            return $upload_image->id;
+        }
+        return FALSE;
     }
 
     /**
